995800083474e4acf06fd0ef4ace4f1b89c75153
Shubham Verma <shubhamv@izooto.com>
2024-06-02 00:28:01 +0530

    change the file structure remove js dependency from project

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
           }
 
     },
-    entry: path.join(__dirname, './src/index.js'),
+    entry: path.join(__dirname, './src/js/Main.ts'),
     output: {
         filename: 'DGstyles.js',
         path: path.resolve(__dirname, 'dist'),
